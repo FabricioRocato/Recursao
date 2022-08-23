ed91adb36f626fe1d3a561f06ce5d938d7d2b480
FabricioRocato <rocatoeua@hotmail.com>
2022-08-22 21:40:54 -0300

    recursaoo

---
+++ Fatorial/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("Fatorial de 4 é" +
+                calcularFatorial(4));
+    }
+        private static Integer calcularFatorial(Integer valor){
+        if (valor == 1)
+            return 1;
+
+            return valor * calcularFatorial(valor - 1);
+    }
+}
